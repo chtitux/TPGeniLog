79389b3a937944b709f1fb1261524efec77c66b2
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-01-13 09:53:12 +0100

    Import inintial

---
+++ src/fr/uha/ensisa/gl/lift/controller/ElevatorControllerImpl.java
@@ -0,0 +1,136 @@
+package fr.uha.ensisa.gl.lift.controller;
+
+import java.util.ArrayList;
+
+import fr.ensisa.uha.ff.gl.lift.hard.Button;
+import fr.ensisa.uha.ff.gl.lift.hard.Door;
+import fr.ensisa.uha.ff.gl.lift.hard.ElevatorController;
+import fr.ensisa.uha.ff.gl.lift.hard.FloorSensor;
+import fr.ensisa.uha.ff.gl.lift.hard.Motor;
+import fr.ensisa.uha.ff.gl.lift.hard.Timer;
+import fr.ensisa.uha.ff.gl.lift.hard.QueryableMotor.MotorStatus;
+
+public class ElevatorControllerImpl implements ElevatorController {
+
+	private Door door;
+	private Motor motor;
+	private Timer timer;
+	private ArrayList<FloorSensor> floorSensors;
+
+	public ElevatorControllerImpl() {
+		this.floorSensors = new ArrayList<FloorSensor>();
+	}
+	
+	@Override
+	public void doorOpened(Door sender) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doorClosed(Door sender) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void motorStatusChanged(Motor sender, MotorStatus status) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void request(Button sender, Integer floor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cabinAtFloor(FloorSensor sender, Integer floor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cabinLeftFloor(FloorSensor sender, Integer floor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void timeout(Timer timer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Door getDoor() {
+		return this.door;
+	}
+
+	@Override
+	public void setDoor(Door door) {
+		this.door = door;
+	}
+
+	@Override
+	public Motor getMotor() {
+		return this.motor;
+	}
+
+	@Override
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+
+	@Override
+	public Button getCabinButton(int floor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Button getFloorButton(int floor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCabinButton(int floor, Button button) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFloorButton(int floor, Button button) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public FloorSensor getFloorSensor(int floor) {
+		if (floor >= this.floorSensors.size())
+			return null;
+		else
+			return this.floorSensors.get(floor);
+	}
+
+	@Override
+	public void setFloorSensor(int floor, FloorSensor sensor) {
+		for (int i = this.floorSensors.size(); i<=floor; i++)
+			this.floorSensors.add(null);
+		
+		this.floorSensors.set(floor, sensor);
+	}
+
+	@Override
+	public Timer getTimer() {
+		return this.timer;
+	}
+
+	@Override
+	public void setTimer(Timer t) {
+		this.timer = t;
+	}
+
+}
---
+++ tests/fr/uha/ensisa/gl/lift/controller/.svn/text-base/AccessorsTest.java.svn-base
@@ -0,0 +1,69 @@
+package fr.uha.ensisa.gl.lift.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.ensisa.uha.ff.gl.lift.hard.Button;
+import fr.ensisa.uha.ff.gl.lift.hard.Door;
+import fr.ensisa.uha.ff.gl.lift.hard.ElevatorController;
+import fr.ensisa.uha.ff.gl.lift.hard.FloorSensor;
+import fr.ensisa.uha.ff.gl.lift.hard.Motor;
+import fr.ensisa.uha.ff.gl.lift.hard.Timer;
+import fr.ensisa.uha.ff.gl.lift.hard.QueryableMotor.MotorStatus;
+
+
+public class AccessorsTest {
+	@Test public void doorAccessor() {
+		ElevatorControllerImpl sut = new ElevatorControllerImpl();
+		Door d = new Door() {
+			@Override
+			public void openDoors() {
+			}
+
+			@Override
+			public void closeDoors() {
+			}};
+		sut.setDoor(d);
+		assertEquals(d, sut.getDoor());
+	}
+	
+	@Test public void motorAccessor() {
+		ElevatorControllerImpl sut = new ElevatorControllerImpl();
+		Motor m = new Motor() {
+			@Override
+			public void goUp() {
+			}
+
+			@Override
+			public void goDown() {
+			}
+
+			@Override
+			public void stopMove() {
+			}};
+		sut.setMotor(m);
+		assertEquals(m, sut.getMotor());
+	}
+	
+	@Test public void timerAccessor() {
+		ElevatorControllerImpl sut = new ElevatorControllerImpl();
+		Timer t = new Timer() {
+			@Override
+			public void countdown(int ms) {
+			}
+
+			@Override
+			public void cancel() {
+			}};
+		sut.setTimer(t);
+		assertEquals(t, sut.getTimer());
+	}
+	
+	@Test public void floorSensorAccessor() {
+		ElevatorControllerImpl sut = new ElevatorControllerImpl();
+		FloorSensor fs = new FloorSensor() {};
+		sut.setFloorSensor(0, fs);
+		assertEquals(fs, sut.getFloorSensor(0));
+	}
+}
