2a048d1e8aa735bfd0cb22affc4d2d295e06b720
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-01-13 11:43:29 +0100

    Ajout du CabinAtFloorTestsEM (Mocks)

---
+++ tests/fr/uha/ensisa/gl/lift/controller/CabinAtFloorTestsEM.java
@@ -0,0 +1,29 @@
+package fr.uha.ensisa.gl.lift.controller;
+
+import org.junit.Before;
+
+import fr.ensisa.uha.ff.gl.lift.hard.Button;
+import fr.ensisa.uha.ff.gl.lift.hard.Door;
+import fr.ensisa.uha.ff.gl.lift.hard.FloorSensor;
+import static org.easymock.EasyMock.*;
+
+public class CabinAtFloorTestsEM {
+	public ElevatorControllerImpl sut;
+	public Door door = createMock(Door.class);
+	public Button cb0 = createMock(Button.class);
+	public Button fb0 = createMock(Button.class);
+	public FloorSensor fs0 = createMock(FloorSensor.class);
+	
+	@Before public void createSut() {
+		this.sut = new ElevatorControllerImpl();
+		this.sut.setDoor(this.door);
+		this.sut.setCabinButton(0, cb0);
+		this.sut.setFloorButton(0, fb0);
+		this.sut.setFloorSensor(0, fs0);
+		
+		reset(door);
+		reset(cb0);
+		reset(fb0);
+		reset(fs0);
+	}
+}
