03151bee67329fbc1a619ca07fb322f20c5f47ab
theophile.helleboid@uha.fr <theophile.helleboid@uha.fr@1e0a195d-9542-9549-8217-13f3b5cd1626>
2011-01-18 10:33:51 +0000

    Implémentation du test de la requête à l'étage 0
    git-svn-id: https://anthurus.univ-mulhouse.fr/svn/gl1011/brunhelleboid/fr.uha.ensisa.gl.lift.controller@204 1e0a195d-9542-9549-8217-13f3b5cd1626

--- tests/fr/uha/ensisa/gl/lift/controller/RequestTest.java
+++ tests/fr/uha/ensisa/gl/lift/controller/RequestTest.java
@@ -1,27 +1,12 @@
 package fr.uha.ensisa.gl.lift.controller;
 
-import static org.easymock.EasyMock.*;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-import fr.ensisa.uha.ff.gl.lift.hard.Button;
-import fr.ensisa.uha.ff.gl.lift.hard.Door;
 
-
-public class RequestTest {
+public class RequestTest extends CabinAtFloorTestsEM {
 	@Test public void request0() {
-		ElevatorControllerImpl mock = createMock(ElevatorControllerImpl.class);
-		ElevatorControllerImpl sut = new ElevatorControllerImpl();
-		
-		Door mockDoor = createMock(Door.class);
-		sut.setDoor(mockDoor);
-		Button mockButton = createMock(Button.class);
-		
-		
-		
-		sut.request(mockButton, 0);
-		
-		//assertTrue(this.openDoorsSent);
+		this.zeroPressed();
 	}
+	
+	
 }
