c4f123d78b2afa8bbe6dcf9982f714420d227357
theophile.helleboid@uha.fr <theophile.helleboid@uha.fr@1e0a195d-9542-9549-8217-13f3b5cd1626>
2011-01-24 13:17:35 +0000

    Tant qu'à faire, autant se baser sur la classe de test qui contient tous les bouchons grâce à l'héritage
    git-svn-id: https://anthurus.univ-mulhouse.fr/svn/gl1011/brunhelleboid/fr.uha.ensisa.gl.lift.controller@425 1e0a195d-9542-9549-8217-13f3b5cd1626

--- tests/fr/uha/ensisa/gl/lift/controller/DoorClosedTest.java
+++ tests/fr/uha/ensisa/gl/lift/controller/DoorClosedTest.java
@@ -9,23 +9,7 @@ import fr.ensisa.uha.ff.gl.lift.hard.Door;
 import fr.ensisa.uha.ff.gl.lift.hard.Motor;
 import fr.ensisa.uha.ff.gl.lift.hard.Timer;
 
-public class DoorClosedTest {
-	public ElevatorControllerImpl sut;
-	public Door door = createMock(Door.class);
-	public Timer timer = createMock(Timer.class);
-	public Motor motor = createMock(Motor.class);
-
-	@Before
-	public void createSut() {
-		sut = new ElevatorControllerImpl();
-		sut.setDoor(door);
-		sut.setTimer(timer);
-		sut.setMotor(motor);
-		reset(door);
-		reset(timer);
-		reset(motor);
-	}
-
+public class DoorClosedTest  extends CabinAtFloorTestsEM {
 	@Test
 	public void normalClose() {
 		timer.cancel();
