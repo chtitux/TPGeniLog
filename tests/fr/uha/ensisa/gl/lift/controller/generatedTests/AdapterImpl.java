65635ac0570e564bfac1254ee27e0b9c5b8a7bf5
theophile.helleboid@uha.fr <theophile.helleboid@uha.fr@1e0a195d-9542-9549-8217-13f3b5cd1626>
2011-01-24 13:15:23 +0000

    Implémentation de ElevatorControllerrequest Conversion de Actor en Button, Motor, Timer
    git-svn-id: https://anthurus.univ-mulhouse.fr/svn/gl1011/brunhelleboid/fr.uha.ensisa.gl.lift.controller@413 1e0a195d-9542-9549-8217-13f3b5cd1626

--- tests/fr/uha/ensisa/gl/lift/controller/generatedTests/AdapterImpl.java
+++ tests/fr/uha/ensisa/gl/lift/controller/generatedTests/AdapterImpl.java
@@ -185,14 +185,37 @@ public class AdapterImpl implements AdapterInterface {
 		return floorNum;
 	}
 	
-	private int getFloorNumber(Floor floor)
-	{
-		int floorNum;
+	private Object getObjectFromActor(Actor sender)	{
+		Object actor = null;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.cb0)
+			actor = this.cb0;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.fb0)
+			actor = this.fb0;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.cb1)
+			actor = this.cb1;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.fb1)
+			actor = this.fb1;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.cb2)
+			actor = this.cb2;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.fb2)
+			actor = this.fb2;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.motor)
+			actor = this.motor;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.clock)
+			actor = this.timer;
+		if (sender == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Actor.door)
+			actor = this.door;
+		
+		return actor;
+	}
+	
+	private int getFloorNumber(Floor floor)	{
+		int floorNum = -1;
 		if (floor == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Floor._0)
 			floorNum = 0;
-		else if (floor == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Floor._1)
+		if (floor == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Floor._1)
 			floorNum = 1;
-		else
+		if (floor == fr.uha.ensisa.gl.lift.controller.generatedTests.ElevatorController.InstancesDefinition.Floor._2)
 			floorNum = 2;
 		return floorNum;
 	}
