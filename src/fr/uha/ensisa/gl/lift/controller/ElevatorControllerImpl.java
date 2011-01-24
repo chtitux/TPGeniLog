bf596af37227bda6408cb35559ab4fbf2c885ee8
David Brun <brundavid@gmail.com>
2011-01-24 13:13:58 +0100

    Suppression du code commenté

--- src/fr/uha/ensisa/gl/lift/controller/ElevatorControllerImpl.java
+++ src/fr/uha/ensisa/gl/lift/controller/ElevatorControllerImpl.java
@@ -14,7 +14,6 @@ import fr.ensisa.uha.ff.gl.lift.hard.MotorListener;
 import fr.ensisa.uha.ff.gl.lift.hard.Timer;
 import fr.ensisa.uha.ff.gl.lift.hard.QueryableMotor.MotorStatus;
 import fr.ensisa.uha.ff.gl.lift.hard.TimerListener;
-import fr.uha.ensisa.gl.lift.controller.generatedTests.Enumerations.Messages;
 
 public class ElevatorControllerImpl
 	implements ElevatorController, DoorListener, MotorListener, ButtonListener, FloorSensorListener, TimerListener {
@@ -77,9 +76,7 @@ public class ElevatorControllerImpl
 	@Override
 	public void doorOpened(Door sender) {
 		this.state = States.Running__DoorOpened__LetInOut;
-		//this.mustGoDown = false;
-		//this.mustGoUp = false;
-		
+
 		if (!isBetweenFloors)
 			this.getTimer().countdown(5000);
 		
@@ -106,7 +103,6 @@ public class ElevatorControllerImpl
 		else if (this.currentFloor == this.requestedFloor && !this.mustGoDown && !this.mustGoUp) {
 			this.mustGoDown = false;
 			this.mustGoUp = false;
-			//this.motor.stopMove();
 		}
 		
 		this.mustStay[currentFloor] = false;
@@ -114,29 +110,7 @@ public class ElevatorControllerImpl
 
 	@Override
 	public void motorStatusChanged(Motor sender, MotorStatus status) {
-		//this.isBetweenFloors = true;
-		//this.motor.stopMove();
-		/*
-		switch(this.state)
-		{
-			case Running__AtFloor:
-				this.state = States.Running__BetweenFloors;
-				break;
-		}
-		
-		switch (status)
-		{
-			case GoingDownward:
-				this.motor.goDown();
-				break;
-			case GoingUpward:
-				this.motor.goUp();
-				break;
-			case Stopped:
-				this.motor.stopMove();
-				break;
-		}
-		*/
+		//Rien pour le moment
 	}
 
 	@Override
@@ -173,51 +147,20 @@ public class ElevatorControllerImpl
 			else if (this.currentFloor == this.requestedFloor && !this.mustGoDown && !this.mustGoUp) {
 				this.mustGoDown = false;
 				this.mustGoUp = false;
-				//this.motor.stopMove();
 			}
 		}
-		
-		
-		//this.requestedFloor = floor;
-		//if (this.currentFloor.compareTo(this.requestedFloor) != 0)
-			//sender.requestACK();
-		/*
-		// Si l'ascenceur est à l'arrêt et
-		// que le dernier étage atteint est l'étage demandé, on ouvre la porte
-		if(this.isMoving == false && this.currentFloor.equals(this.requestedFloor)) {
-			this.door.openDoors();
-			//sender.requestServiced();
-		} else {
-			if(!this.doorClosed)
-			// Si les portes ne sont pas ouvertes, on les fermes
-				this.door.closeDoors();
-			
-			if(this.currentFloor.compareTo(this.requestedFloor) < 0) {
-				// L'étage actuel est plus petit que demandé, il faut monter
-				this.motor.goUp();
-			} else { // Sinon, il faut descendre
-				this.motor.goDown();
-			}
-		}
-		*/
 	}
 
 	@Override
 	public void cabinAtFloor(FloorSensor sender, Integer floor) {
 		this.state = States.Running__AtFloor;
 		
-		//On notifie l'arrivée de l'ascenseur aux boutons de la cabine et de l'étage,
-		//mais que si on vient de changer d'étage
-		if (this.currentFloor != floor)
-		{
-			//this.getCabinButton(floor).requestServiced();
-			//this.getFloorButton(floor).requestServiced();
-		}
-		
 		//On met à jour le numéro de l'étage actuel
 		this.currentFloor = floor;
 		
 		//On stoppe le moteur si on est arrivé et on demande l'ouverture des portes
+		//On notifie l'arrivée de l'ascenseur aux boutons de la cabine et de l'étage,
+		//mais que si on vient de changer d'étage
 		if(this.currentFloor == this.requestedFloor) {
 			this.motor.stopMove();
 			this.door.openDoors();
@@ -329,11 +272,7 @@ public class ElevatorControllerImpl
 	public void setTimer(Timer t) {
 		this.timer = t;
 	}
-	/*
-	public boolean isDoorClosed() {
-		return doorClosed;
-	}
-	*/
+	
 	public void setState(States state) {
 		this.state = state;
 	}
