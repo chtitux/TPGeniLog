b94a5a3130d46fe23dcd614d7b9fb3834c500f0a
David Brun <brundavid@gmail.com>
2011-01-24 11:08:46 +0100

    Tant qu'à faire, autant se baser sur la classe de test qui contient tous les bouchons grâce à l'héritage

--- tests/fr/uha/ensisa/gl/lift/controller/DoorOpenedTest.java
+++ tests/fr/uha/ensisa/gl/lift/controller/DoorOpenedTest.java
@@ -12,34 +12,7 @@ import fr.ensisa.uha.ff.gl.lift.hard.Motor;
 import fr.ensisa.uha.ff.gl.lift.hard.QueryableMotor.MotorStatus;
 import fr.ensisa.uha.ff.gl.lift.hard.Timer;
 
-public class DoorOpenedTest {
-	public ElevatorControllerImpl sut;
-	public Door door = createMock(Door.class);
-	public Button cb0 = createMock(Button.class);
-	public Button fb0 = createMock(Button.class);
-	public Button fb2 = createMock(Button.class);
-	public Button cb2 = createMock(Button.class);
-	public FloorSensor fs0 = createMock(FloorSensor.class);
-	public Motor motor = createMock(Motor.class);
-	public Timer timer = createMock(Timer.class);
-
-	@Before
-	public void createSut() {
-		sut = new ElevatorControllerImpl();
-		sut.setDoor(door);
-		sut.setCabinButton(0, cb0);
-		sut.setFloorButton(0, fb0);
-		sut.setFloorSensor(0, fs0);
-		sut.setMotor(motor);
-		sut.setTimer(timer);
-		reset(door);
-		reset(cb0);
-		reset(fb0);
-		reset(fs0);
-		reset(motor);
-		reset(timer);
-	}
-
+public class DoorOpenedTest extends CabinAtFloorTestsEM {
 	@Test
 	public void normalOpen() {
 		timer.countdown(5000);
--- tests/fr/uha/ensisa/gl/lift/controller/SensorsTest.java
+++ tests/fr/uha/ensisa/gl/lift/controller/SensorsTest.java
@@ -12,41 +12,7 @@ import fr.ensisa.uha.ff.gl.lift.hard.Motor;
 import fr.ensisa.uha.ff.gl.lift.hard.QueryableMotor.MotorStatus;
 import fr.ensisa.uha.ff.gl.lift.hard.Timer;
 
-public class SensorsTest {
-	public ElevatorControllerImpl sut;
-	public Door door = createMock(Door.class);
-	public Timer timer = createMock(Timer.class);
-	public Motor motor = createMock(Motor.class);
-	
-	// Étage 0
-	public FloorSensor fs0 = createMock(FloorSensor.class);
-	// Étage 1
-	public Button cb1 = createMock(Button.class);
-	public Button fb1 = createMock(Button.class);
-	public FloorSensor fs1 = createMock(FloorSensor.class);
-	
-	@Before
-	public void createSut() {
-		this.sut = new ElevatorControllerImpl();
-		this.sut.setDoor(this.door);
-		this.sut.setTimer(timer);
-		this.sut.setMotor(this.motor);
-		// Etage 0
-		this.sut.setFloorSensor(0, fs0);
-		// Etage 1
-		this.sut.setCabinButton(1, cb1);
-		this.sut.setFloorButton(1, fb1);
-		this.sut.setFloorSensor(1, fs1);
-		
-		reset(door);
-		reset(timer);
-		reset(motor);
-		reset(fs0);
-		reset(cb1);
-		reset(fb1);
-		reset(fs1);
-	}
-
+public class SensorsTest extends CabinAtFloorTestsEM {
 	@Test
 	public void leftFloor() {
 		//D'abord on request l'étage 1
