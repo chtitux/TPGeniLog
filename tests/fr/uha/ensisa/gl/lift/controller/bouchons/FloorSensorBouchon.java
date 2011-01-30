77e80c7dd0987f71616245946065110042837155
David Brun <brundavid@gmail.com>
2011-01-31 00:26:40 +0100

    Suppression des fichiers déplacés

--- tests/fr/uha/ensisa/gl/lift/controller/bouchons/FloorSensorBouchon.java
+++
@@ -1,7 +0,0 @@
-package fr.uha.ensisa.gl.lift.controller.bouchons;
-
-import fr.ensisa.uha.ff.gl.lift.hard.FloorSensor;
-
-public class FloorSensorBouchon extends Bouchon implements FloorSensor {
-
-}
